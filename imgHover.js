a0ae5301dc8387af568886714051c255db3066f6
Nexus <git.nexus9@gmail.com>
2025-01-13 10:48:49 +0545

    second commit

--- imgHover.js
+++ imgHover.js
@@ -1,30 +1,19 @@
 let characters = document.querySelectorAll(".characters");
-// let image = document.querySelector(".image");
 
-// characters.addEventListener("mousemove", function(move){
-//     image.style.left = move.x + "px";
-//     image.style.top = move.y + "px";
-// })
-// characters.addEventListener("mouseenter", function(move){
-//     image.style.opacity = 1;
-// })
-// characters.addEventListener("mouseleave", function(move){
-//     image.style.opacity = 0;
-// })
+characters.forEach(function (image) {
+  image.addEventListener("mouseenter", function () {
+    image.children[1].style.opacity = 1;
+    image.children[1].style.height = "150px";
+    image.children[1].style.width = "150px";
+  });
 
-characters.forEach(function(image){
+  image.addEventListener("mouseleave", function () {
+    image.children[1].style.opacity = 0;
+  });
 
-    image.addEventListener("mouseenter", function(){
-        image.children[1].style.opacity = 1;
-    })
-
-    image.addEventListener("mouseleave", function(){
-        image.children[1].style.opacity = 0;
-    })
-
-    image.addEventListener("mousemove", function(move){
-        image.children[1].style.left = move.clientX + "px";
-        image.children[1].style.top = move.clientY + "px";
-
-    })
-})
+  image.addEventListener("mousemove", function (move) {
+    const rect = image.getBoundingClientRect();
+    image.children[1].style.left = move.clientX - rect.left + "px";
+    image.children[1].style.top = move.clientY - rect.top + "px";
+  });
+});
